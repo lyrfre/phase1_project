42e42c191f25be6956af1da96fbc5e1879826d2c
Lyra Freund <lyrafreund@gmail.com>
2023-09-27 12:49:12 -0600

    added new json, embedded json, commented out html, confused with foreach

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 document.addEventListener("DOMContentLoaded", ()=> {
-    fetch("http://localhost:3000/artists")
+    fetch("http://localhost:3000/art")
     .then(response => response.json())
-    .then(response => {
-        featuredArtist(response["2"])
-        //globalArtist = response["0"]
-        response.forEach(console.log(artistCard))
-    })
+        .then(data => {
+            console.log(data)
+
+            })
+    }) 
+// })
+
+//         featuredArtist(response["2"])
+//         //globalArtist = response["0"]
+//  //       response.forEach(console.log(artistCard))
+//         let georgia = response["3"]
+//         let console = georgia.forEach(featuredArtist(["5"]["0"]))
+//         console.log(console)
+    //})
     // fetch("http://localhost:3000/artists/1")
     // .then(response => response.json())
     // .then(response => {
     //     console.log(response)
     // })
-})
+
 
 let globalArtist
 
@@ -20,57 +29,42 @@ function clickEvent1(art){
 }
 
 
-function featuredArtist1(art){
-//featured artist input function
-    const featuredName = document.querySelector("#featured-artist-name")
-    const image = document.createElement("img")
-    image.className = ""
-    const firstImage = document.querySelector("#first-image")
-    const firstTitle = document.querySelector("#first-title")
-    const firstDate = document.querySelector("#first-date")
+// function featuredArtist1(art){
+// //featured artist input function
+//     const featuredName = document.querySelector("#featured-artist-name")
+//     const image = document.createElement("img")
+//     image.className = ""
+//     const firstImage = document.querySelector("#first-image")
+//     const firstTitle = document.querySelector("#first-title")
+//     const firstDate = document.querySelector("#first-date")
 
-    const secondImage = document.querySelector("#second-image")
-    const secondTitle = document.querySelector("#second-title")
-    const secondDate = document.querySelector("#second-date")
+//     const secondImage = document.querySelector("#second-image")
+//     const secondTitle = document.querySelector("#second-title")
+//     const secondDate = document.querySelector("#second-date")
     
-    const thirdImage = document.querySelector("#third-image")
-    const thirdTitle = document.querySelector("third-title")
-    const thirdDate = document.querySelector("#third-date")
+//     const thirdImage = document.querySelector("#third-image")
+//     const thirdTitle = document.querySelector("third-title")
+//     const thirdDate = document.querySelector("#third-date")
 
-    featuredName.textContent = art.name
+//     featuredName.textContent = art.name
     
-    firstImage.src = art.artworks.piece1.image
-    firstTitle.textContent = art.artworks.piece1.title
-    firstDate.textContent = art.artworks.piece1.year
+//     firstImage.src = art.artworks.piece1.image
+//     firstTitle.textContent = art.artworks.piece1.title
+//     firstDate.textContent = art.artworks.piece1.year
 
-    secondImage.src = art.artworks.piece2.image
-    secondTitle.textContent = art.artworks.piece2.title
-    secondDate.textContent = art.artworks.piece2.year
+//     secondImage.src = art.artworks.piece2.image
+//     secondTitle.textContent = art.artworks.piece2.title
+//     secondDate.textContent = art.artworks.piece2.year
 
-    thirdImage.src = art.artworks.piece3.image
-    thirdTitle.textContent = art.artworks.piece3.title
-    thirdDate.textContent = art.artworks.piece3.year
+//     thirdImage.src = art.artworks.piece3.image
+//     thirdTitle.textContent = art.artworks.piece3.title
+//     thirdDate.textContent = art.artworks.piece3.year
 
-    globalArtist = art
-}
-
-function featuredArtist(artwork){
-    const artworkDiv = document.createElement("div")
-    const artworkImage = document.createElement("img")
-    const artworkTitle = document.createElement("p")
-    const artworkDate = document.createElement("h3")
-
-    artworkDiv.className = "featured-artwork"
-    artworkImage.src = artwork.image
-    artworkTitle.textContent = artwork.title
-    artworkDate.textContent = artwork.year
-
-    artworkDiv.append(artworkImage, artworkTitle, artworkDate)
-
-    document.querySelector(".detailedWorks").appendChild(artworkDiv)
-}
+//     globalArtist = art
+// }
 
 function artistCard(artist){
+    globalArtist = artist
     const artistDiv = document.createElement("div")
     const artistImage = document.createElement("img")
     const artistName = document.createElement("p")
@@ -78,7 +72,7 @@ function artistCard(artist){
     const artistIdentity = document.createElement("p")
 
     artistDiv.className = "artist-card"
-    artistImage.scr = artist["artist_image"]
+    artistImage.src = artist["artist_image"]
     artistName.textContent = artist.name
     artistYearsAlive.textContent = artist.yearsAlive
     artistIdentity.textContent = artist.nationality
@@ -87,7 +81,48 @@ function artistCard(artist){
 
     document.querySelector(".artist-container").append(artistDiv)
 
-    artistImage.addEventListener("click", (e) => featuredArtist(artist, e))
+    artistImage.addEventListener("click",  artist.artworks.forEach(artwork => featuredArtist(artwork)))
 }
+
+function featuredArtist(artwork){
+    console.log(artwork)
+    // const artworkDiv = document.createElement("div")
+    // const artworkImage = document.createElement("img")
+    // const artworkTitle = document.createElement("p")
+    // const artworkDate = document.createElement("h3")
+
+    // artworkDiv.className = "featured-artwork"
+    // artworkImage.src = artwork.image
+    // artworkTitle.textContent = artwork.title
+    // artworkDate.textContent = artwork.year
+
+    // artworkDiv.append(artworkImage, artworkTitle, artworkDate)
+
+
+    // document.querySelector(".detailedWorks").appendChild(artworkDiv)
+
+    artist.artworks.forEach(art => {
+        const artworkDiv = document.createElement("div")
+        const artworkImage = document.createElement("img")
+        const artworkTitle = document.createElement("p")
+        const artworkDate = document.createElement("h3")
+    
+        artworkDiv.className = "featured-artwork"
+        artworkImage.src = artworks.image
+        artworkTitle.textContent = artworks.title
+        artworkDate.textContent = artworks.year
+    
+        artworkDiv.append(artworkImage, artworkTitle, artworkDate)
+    
+    
+        document.querySelector(".detailedWorks").appendChild(artworkDiv)
+
+    })
+}
+
+
+
+
+//array.forEach(function(currentValue, index, arr), thisValue)
 
 
